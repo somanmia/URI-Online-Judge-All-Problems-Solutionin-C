c3ca1eca547ba306cceac29a41dd42782a787f4b
Md:Soman Mia <59284020+somanmia@users.noreply.github.com>
2020-11-23 23:49:44 -0800

    2344 - Notas da Prova

---
+++ URI_2344.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main(){
+int num;
+while(scanf("%d",&num)!=EOF){
+    if(num==0){
+        printf("E\n");
+    }else if(num>=1 && num<=35){
+        printf("D\n");
+    }else if(num>=36 && num<=60){
+       printf("C\n");
+    }else if(num>=61 && num<=85){
+       printf("B\n");
+    }else if(num>=86 && num<=100){
+       printf("A\n");
+    }
+}
+
+
+
+return 0;
+}
+
